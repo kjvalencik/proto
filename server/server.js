717b98a03adfdc185cbc1db4c6ce8ea2ac139ded
K.J. Valencik <kjvalencik@gmail.com>
2013-04-25 01:51:56 +0400

    Added static caching, but apparently that's deprecated...

--- server/server.js
+++ server/server.js
@@ -63,6 +63,7 @@ function initMiddleware() {
 
 		// Static routes
 		app.use(express.compress());
+		app.use(express.staticCache());
 		app.use(express.static(__dirname + '/../public'));
 
 		// Enable passport
